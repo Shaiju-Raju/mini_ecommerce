f09faae2be5c3aa42e3d365f30b4f8d6ce295d66
Shaiju Raju <shaijur2014@gmail.com>
2026-04-22 10:35:16 +0530

    DB config Changed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,24 @@
 import pkg from 'pg';
 import dotenv from 'dotenv'
-import dns from "dns";
+// import dns from "dns";
 
 dotenv.config();
 
 const { Pool } = pkg;
 
 // 🔥 Force Node to prefer IPv4 globally
-dns.setDefaultResultOrder("ipv4first");
+// dns.setDefaultResultOrder("ipv4first");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432, // default PostgreSQL port
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
     ssl: {
         rejectUnauthorized: false
     },
-    family: 4,
+    // family: 4,
 });
 
 export default pool
